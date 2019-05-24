1925a28e8a442ee51677c7117c9f9220e220748c
joshermar <joshermar@gmail.com>
2019-05-24 13:03:18 -0500

    Added some test log messages

--- heroku_test.py
+++ heroku_test.py
@@ -1,16 +1,19 @@
+import logging
 from flask import Flask, render_template
 
 app = Flask(__name__)
+logging.basicConfig(level=logging.DEBUG)
 
 
 @app.route("/")
-# @app.route("/home")
+@app.route("/home")
 def home():
     return render_template("home.html")
 
 
 @app.route("/about")
 def about():
+    logging.debug('****TEST LOG MESSAGE****')
     return render_template("about.html", title="about")
 
 
